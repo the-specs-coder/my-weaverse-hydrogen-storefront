d9c8e119882d10855e96ab0e7174ecb78c29d70b
the-specs-coder <nhsanjay24@gmail.com>
2025-10-08 19:21:04 +0530

    Text added

--- app/sections/hero-video.tsx
+++ app/sections/hero-video.tsx
@@ -185,6 +185,7 @@ export default function HeroVideo(props: HeroVideoProps) {
         />
         <div ref={scope} className={clsx(variants({ gap }))}>
           {children}
+          Helllooooo
         </div>
       </div>
     </section>
